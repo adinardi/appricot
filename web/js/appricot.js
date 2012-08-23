bfee6ff9de02519d0d167a8b3c18dfb13b5ba1ab
Angelo DiNardi <angelo@dinardi.name>
2012-08-22 23:17:03 -0700

    Improved newline support.

--- web/js/appricot.js
+++ web/js/appricot.js
@@ -477,7 +477,7 @@ appricot.Post = Class.$extend({
                 .addClass('post row-fluid')
                 .html(Mustache.render("<div class='span1'><img class='avatar' src='{{post.user.avatar_image.url}}' /></div><div class='span10'><div class='row-fluid'><div class='span5'><b><a href='https://alpha.app.net/{{post.user.username}}' target='_blank'>{{post.user.username}}</a></b></div><div class='span6'><a href='https://alpha.app.net/{{post.user.username}}/post/{{post.id}}' target='_blank'>{{date}}</a></div></div><div class='row-fluid post_content'>{{&content}}</div></div>", {
                     post: this.post,
-                    content: content,
+                    content: content.replace("\n", "<br>"),
                     date: date.toDateString() + " " + date.toLocaleTimeString()
                 }));
         }
